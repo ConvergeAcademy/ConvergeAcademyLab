b8712cba45172d054671adbe282e73c5d21a3490
ConvergeAcademy <convergeacademy@outlook.com>
2017-04-12 07:53:54 +0530

    ConvergeMusicStore

---
+++ ConvergeMusicStore/ConvergeMusicStoreDAL/AccountDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvergeMusicStoreDAL
+{
+    public class AccountDAL
+    {
+        public  void CreateUser(User user)
+        {
+            MyMusicStoreEntities dbContext = new MyMusicStoreEntities();
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ ConvergeMusicStore/TestProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConvergeMusicStoreDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateUser();
+        }
+
+        public static void CreateUser()
+        {
+            
+
+            User user = new User();
+
+            user.UserName = "Asim";
+            user.Password = "Asim";
+            user.Email = "Asim@gmail.com";
+            user.CreatedDate = System.DateTime.UtcNow;
+
+            AccountDAL dal = new AccountDAL();
+            dal.CreateUser(user);
+        }
+    }
+}
